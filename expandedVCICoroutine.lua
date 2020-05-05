e2314867a3d7106c080e949496051f010cf99896
chinng-inta <58819032+chinng-inta@users.noreply.github.com>
2020-05-05 17:04:09 +0900

    初版作成
    VCIで使用可能なluaのコルーチン機能を拡張するモジュール

---
+++ expandedVCICoroutine.lua
@@ -0,0 +1,33 @@
+local functionTable = {}
+
+local function start(func, ...)
+    local co = coroutine.create(func)
+    local pair = { co = co, ret = nil }
+    
+    pair.ret = coroutine.resume(co, ...)
+    table.insert(functionTable, pair)
+end
+
+local function update()
+    local cnt = 0
+    for i = 1, #functionTable do
+        local co = functionTable[i].co
+        local status = coroutine.status(co) 
+        if status == "dead" then
+            local c = table.remove(functionTable, i)
+            table.insert(functionTable, 1, c)
+            cnt = cnt + 1
+        elseif status ~= "running" then
+            functionTable[i].ret = coroutine.resume(co, functionTable[i].ret)
+        end
+    end
+    for i = 1, cnt do
+        table.remove(functionTable, 1)
+    end
+end
+
+-- VScodeのインテリセンス(入力予測)を使用するのに必要
+return {
+    start = start,
+    update = update
+}
